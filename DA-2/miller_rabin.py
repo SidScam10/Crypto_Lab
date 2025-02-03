2ed1eedb6dfddba2d72f55dc161ace45e694e360
SidScam10 <siddhanth.monnappa@gmail.com>
2025-02-03 18:52:24 +0530

    code changes

--- DA-2/miller_rabin.py
+++ DA-2/miller_rabin.py
@@ -2,8 +2,8 @@ n=int(input("Enter n value: "))
 a=int(input("Enter Base Value: "))
 n_minus=n-1
 k=0
-while(n_minus!=0):
-    n_minus=n_minus//2
+while(n_minus % 2 == 0):
+    n_minus = n_minus // 2
     k+=1
 n_minus=n-1
 m=n_minus//(2**k)
@@ -11,8 +11,7 @@ T=(a**m)%n
 def miller_rabin_test(T):
     if(T==-1 or T==1):
         return "Prime"
-    for i in range(1,k):
-        print(T)
+    for i in range(k):
         T=(T**2)%n
         if(T==-1):
             return "Prime"
