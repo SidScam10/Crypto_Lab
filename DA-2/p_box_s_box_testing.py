3b6bf66ce8b05b41d4eb5cc2b2817948061bd339
SidScam10 <siddhanth.monnappa@gmail.com>
2025-02-08 14:15:57 +0530

    code formatting

--- DA-2/p_box_s_box_testing.py
+++ DA-2/p_box_s_box_testing.py
@@ -10,16 +10,16 @@ def s_box_substitution(bits, s_boxes):
         output.extend(f"{s_boxes[i][row][col]:02b}")
     return list(map(int, output))
 
-bits = list(map(int, input("Enter 8 space-separated values for bits: ").split()))
+bits = list(map(int, input("Enter 8 bits: ").split()))
 
-p8_table = list(map(int, input("Enter 8 space-separated values for P8 Table: ").split()))
+p8_table = list(map(int, input("Enter 8 values for P8 Table: ").split()))
 
 s_boxes = []
-print("Enter values for S-boxes (space-separated for each row):")
+print("Enter values for S-boxes:")
 for i in range(2):
     s_box = []
     for j in range(4):
-        s_box.append(list(map(int, input(f"Enter 4 space-separated values for S-box {i+1} row {j}: ").split())))
+        s_box.append(list(map(int, input(f"Enter 4 values for S-box {i+1} row {j}: ").split())))
     s_boxes.append(s_box)
 
 permuted_bits = p8_permutation(bits, p8_table)
